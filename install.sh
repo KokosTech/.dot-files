b547b7822626d2dde294ee5dd0e3750c14775730
Kaloyan Doychinov <kokolia1070@gmail.com>
2022-06-05 15:15:04 +0300

    [ADD] home .dotfiles

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dot-files/.zshrc ~/.zshrc
+ln -s ~/.dot-files/.zprofile ~/.zprofile
+ln -s ~/.dot-files/.vimrc ~/.vimrc
+ln -s ~/.dot-files/.viminfo ~/.viminfo
+ln -s ~/.dot-files/.gitconfig ~/.gitconfig
+ln -s ~/.dot-files/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
